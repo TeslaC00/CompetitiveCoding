bd748d1af557bc5512bac7dd15505fcb2ef4be7d
TeslaC00 <111948419+TeslaC00@users.noreply.github.com>
2025-12-06 00:28:12 +0530

    Add: Linked list question 1

---
+++ leetcode/Linked List/q1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+struct ListNode {
+	int val;
+	ListNode* next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+ListNode* reverseList(ListNode* head) {
+	ListNode* prev = nullptr;
+	ListNode* curr = head;
+
+	while (curr != nullptr)
+	{
+		ListNode* next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+
+	return prev;
+}
+
+int main() {
+
+	return 0;
+}
